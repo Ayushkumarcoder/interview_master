9a01191eb2adb7ded92a3479f7751cc6ce32a75d
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-04-20 17:39:42 +0530

    completed the AuthForm and FormField made some changes in FormField to make it more reusable and // added a new prop to AuthForm to make it more reusable

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import { FormControl, FormDescription, FormLabel, FormItem, FormMessage } from './ui/form';
 import { Input } from './ui/input';
+import { Controller, FieldValue, FieldValues, Path, Control } from 'react-hook-form';
 
-const FormField = () => (
-    <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-)
+interface FormFeildProps<T extends FieldValues> {
+    name: Path<T>;
+    control: Control<T>;
+    label: string;
+    placeholder: string;
+    type?: "text" | "email" | "password" | "file";
+}
 
-export default FormField
+const FormField = <T extends FieldValues>({
+    name,
+    control,
+    label,
+    placeholder,
+    type = "text",
+}: FormFeildProps<T>) => (
+    <Controller
+        name={name}
+        control={control}
+        
+        render={({ field }) => (
+            <FormItem>
+                <FormLabel>{label}</FormLabel>
+                <FormControl>
+                    <Input className='input' placeholder={placeholder} type={type} {...field} />
+                </FormControl>
+                <FormDescription>
+                    
+                </FormDescription>
+                <FormMessage />
+            </FormItem>
+        )}
+    />
+);
+
+export default FormField;
