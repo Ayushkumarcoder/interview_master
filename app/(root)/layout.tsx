a32840b2de1844c4a8a1e984ff397c8d71ddc9ba
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-04-20 18:30:55 +0530

    basic homepage setting

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,17 @@
+import Link from 'next/link'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+        <nav>
+            <Link href='/' className='flex items-center gap-4'>
+                <img src='/logo.svg' alt='logo' width={38} height={32}/>
+                <h2 className='text-primary-100'>PrepPal</h2>
+            </Link>
+        </nav>
+        {children}
+    </div>
   )
 }
 
