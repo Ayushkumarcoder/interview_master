7957d0a6e098648bdc79def0e58a131cd0c1050a
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-06-24 19:09:59 +0530

    fixed the error of useRouter changed the import from 'next/router' to 'next/navigation'

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { set } from "zod";
 import {vapi} from "@/lib/vapi.sdk";
 
